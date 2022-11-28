796b9d012410d90762046fd7ea90a0c00d1ec9df
Chanjae Lee <103395394+Chanjae-Lee@users.noreply.github.com>
2022-11-28 17:47:06 +0900

    Update logger.py

--- logger.py
+++ logger.py
@@ -13,6 +13,7 @@ import torch.utils.tensorboard as tb
 #         summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
 #         self.writer.add_summary(summary, step)
 
+
 class Logger(object):
     def __init__(self, log_dir):
         """ Create a summary writer object logging to log_dir."""
